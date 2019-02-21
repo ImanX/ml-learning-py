ad42c26e51453e4aa20aa654ffc90762557a1d4c
imanx <Imannamix@gmail.com>
2019-02-21 11:52:03 +0330

    change

--- venv/FirstSessionLearning.py
+++ venv/FirstSessionLearning.py
@@ -1,12 +1,18 @@
 letter = "hello World. you are Ok. yes OK"
-
-splited = letter.split(".");
-newLetter = "";
-print (letter)
-for i in range(len(splited)):
-    newLetter += splited[i].strip().capitalize();
-    if (i+1) != len(splited):
-        newLetter+=". "
+x = "Hello world";
 
 
-print (newLetter)
+def toLetter(let):
+    splited = let.split(".");
+    newLetter = "";
+    print (letter)
+    for i in range(len(splited)):
+        newLetter += splited[i].strip().capitalize();
+        if (i + 1) != len(splited):
+            newLetter += ". "
+    return letter;
+
+
+print (toLetter(letter))
+
+print(x[x.find("o"):len(x)]);
