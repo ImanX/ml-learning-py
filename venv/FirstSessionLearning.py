f51119a682e28a3a26910c3d3af20e67bf232739
imanx <Imannamix@gmail.com>
2019-02-26 09:59:52 +0330

    commit

--- venv/FirstSessionLearning.py
+++ venv/FirstSessionLearning.py
@@ -1,18 +1,20 @@
-letter = "hello World. you are Ok. yes OK"
-x = "Hello world";
+
+
+
+
 
 
 def toLetter(let):
     splited = let.split(".");
     newLetter = "";
-    print (letter)
     for i in range(len(splited)):
         newLetter += splited[i].strip().capitalize();
         if (i + 1) != len(splited):
             newLetter += ". "
-    return letter;
+    return newLetter;
 
 
-print (toLetter(letter))
 
-print(x[x.find("o"):len(x)]);
+print(toLetter("this is python. this's a powerful language. it's response from a function. wow"))
+
+# print(x[x.find("o"):len(x)]);
